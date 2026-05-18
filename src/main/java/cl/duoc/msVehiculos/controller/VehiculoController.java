c9fa53e14f44cc7d0974307893eb8b404288588d
Dafex96 <davicolo717@gmail.com>
2026-05-18 18:00:27 -0400

    listoListo

--- src/main/java/cl/duoc/msVehiculos/controller/VehiculoController.java
+++ src/main/java/cl/duoc/msVehiculos/controller/VehiculoController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import cl.duoc.msVehiculos.dto.VehiculoDTO;
 import cl.duoc.msVehiculos.model.MarcaVehiculo;
 import cl.duoc.msVehiculos.model.TipoVehiculo;
