c2f435984b0b9985c5c462c14658eeec4c849085
Dafex96 <davicolo717@gmail.com>
2026-06-08 22:55:57 -0400

    alDiaCreo

--- src/main/java/cl/duoc/msVehiculos/model/Vehiculo.java
+++ src/main/java/cl/duoc/msVehiculos/model/Vehiculo.java
@@ -1,5 +1,6 @@
 package cl.duoc.msVehiculos.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,29 +18,37 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "vehiculo")
+@Schema(description = "Representa un vehículo en el sistema")
 public class Vehiculo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID del vehículo")
     private Integer idVehiculo;
 
     @Column(nullable = false)
+    @Schema(description = "Patente del vehículo")
     private String patente;
 
     @Column(nullable = false)
+    @Schema(description = "Año del vehículo")
     private Integer anio;
 
     @Column(nullable = false)
+    @Schema(description = "Color del vehículo")
     private String color;
 
     @Column(nullable = false)
+    @Schema(description = "Kilometraje del vehículo")
     private Integer kilometraje;
 
     @ManyToOne
     @JoinColumn(name = "idMarca")
+    @Schema(description = "Marca del vehículo")
     private MarcaVehiculo marcaVehiculo;
 
     @ManyToOne
     @JoinColumn(name = "idTipoVehiculo")
+    @Schema(description = "Tipo de vehículo")
     private TipoVehiculo tipoVehiculo;
 }
