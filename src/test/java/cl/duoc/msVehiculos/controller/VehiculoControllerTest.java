f83f89c94c1932fadd09753a77e8af8d11c03a45
Dafex96 <davicolo717@gmail.com>
2026-06-16 18:23:16 -0400

    mensaje

--- src/test/java/cl/duoc/msVehiculos/controller/VehiculoControllerTest.java
+++ src/test/java/cl/duoc/msVehiculos/controller/VehiculoControllerTest.java
@@ -1,12 +1,21 @@
 package cl.duoc.msVehiculos.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;  
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import cl.duoc.msVehiculos.model.MarcaVehiculo;
@@ -48,8 +57,6 @@ public class VehiculoControllerTest {
                      .andExpect(status().isOk());
        }
 
-
-
        @Test
        void buscarPorId_retorna404() throws Exception {
               // ARRANGE: este servicio debe lanzar una excepción
@@ -60,8 +67,75 @@ public class VehiculoControllerTest {
                      .andExpect(status().isNotFound());
        }
 
+       @Test
+       void listarVehiculos_retorna200() throws Exception {
+              
+              List<Vehiculo> listaFalsa = new ArrayList<>();
+              listaFalsa.add(vehiculoEjemplo);
 
+              // ARRANGE: este servicio debe retornar una lista de vehiculos
+              when(vehiculoService.listarVehiculos()).thenReturn(listaFalsa);
 
+              llamadaFalsa.perform(get("/api/v1/vehiculos/listar_vehiculo"))
+                     .andExpect(status().isOk())
+                     .andExpect(jsonPath("$[0].patente").value("ABC123"));
+       }
+
+       @Test
+       void listarVehiculos_retorna204() throws Exception {
+              // ARRANGE: lista vacía
+              List<Vehiculo> listaVacia = new ArrayList<>();
+              when(vehiculoService.listarVehiculos()).thenReturn(listaVacia);
+              
+              // ACT + ASSERT: verificamos que retorna 204 sin contenido
+              llamadaFalsa.perform(get("/api/v1/vehiculos/listar_vehiculo"))
+                            .andExpect(status().isNoContent()); // código HTTP 204
+       }
+
+       @Test
+       void guardar_retorna200() throws Exception {
+              // ARRANGE
+              when(vehiculoService.agregarVehiculo(any(Vehiculo.class))).thenReturn(vehiculoEjemplo);
+
+              // ACT + ASSERT
+              llamadaFalsa.perform(post("/api/v1/vehiculos")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("{\"id\":1,\"patente\":\"ABC123\",\"marcaVehiculo\":{\"id\":1,\"nombre\":\"Toyota\"},\"anio\":2020,\"color\":\"Rojo\",\"kilometraje\":2342342,\"tipoVehiculo\":{\"id\":1,\"nombre\":\"Auto\"}}"))
+                     .andExpect(status().isOk())
+                     .andExpect(jsonPath("$.patente").value("ABC123"));
+       }
+
+       @Test
+       void eliminar_retorna204() throws Exception {
+        // ARRANGE: el servicio no hace nada (método void)
+              doNothing().when(vehiculoService).eliminarVehiculo(1);
+
+        // ACT + ASSERT
+              llamadaFalsa.perform(delete("/api/v1/vehiculos/1"))
+                     .andExpect(status().isNoContent());
+       }
+
+       @Test
+       void eliminar_retorna404() throws Exception {
+              // ARRANGE: el servicio lanza excepción
+              doThrow(new RuntimeException("Vehiculo no existe")).when(vehiculoService).eliminarVehiculo(99);
+
+              // ACT + ASSERT
+              llamadaFalsa.perform(delete("/api/v1/vehiculos/99"))
+                            .andExpect(status().isNotFound());
+       }
+
+       @Test
+       void obtenerDoctorDTO_retorna200() throws Exception {
+              // ARRANGE
+              when(vehiculoService.buscarPorId(1)).thenReturn(vehiculoEjemplo);
+
+              // ACT + ASSERT
+              llamadaFalsa.perform(get("/api/v1/vehiculos/dto/1"))
+                     .andExpect(status().isOk())
+                     .andExpect(jsonPath("$[0].patente").value("ABC123"))
+                     .andExpect(jsonPath("$[0].marcaVehiculo.nombre").value("Toyota"));
+       }
 
 
 }
