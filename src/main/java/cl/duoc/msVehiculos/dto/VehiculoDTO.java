3590c0bed436ce7003608410a1da68a565701268
Dafex96 <davicolo717@gmail.com>
2026-06-23 16:51:16 -0400

    minor fix

--- src/main/java/cl/duoc/msVehiculos/dto/VehiculoDTO.java
+++ src/main/java/cl/duoc/msVehiculos/dto/VehiculoDTO.java
@@ -1,5 +1,6 @@
 package cl.duoc.msVehiculos.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Representa un vehículoDTO con sus atributos principales")
 public class VehiculoDTO {
 
+    @Schema(description = "ID del vehículo")
     private Integer id;
+    
+    @Schema(description = "Patente del vehículo")
     private String patente;
+    
+    @Schema(description = "Marca del vehículo")
     private String marca;
+
+    @Schema(description = "Modelo del vehículo")
     private String modelo;
 
 }
